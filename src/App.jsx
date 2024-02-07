1b37a03a7a70d073a177ab593571f43ecb7b8e14
AMMULU <rishitha.gajula2002@gmail.com>
2024-02-07 20:45:13 +0530

    Quiz app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Quizapp from './components/Quizapp'
+
+function App() {
+  return (
+      <>
+      <Quizapp/>
+      </>
+  )
+}
+
+export default App
---
+++ src/components/data.js
@@ -0,0 +1,47 @@
+export const data = [
+    {
+        question: " HTML stands for",
+        option1: "HighText Machine Language",
+        option2: "HyperText and links Markup Language",
+        option3: "HyperText Markup Language",
+        option4: "None of these",
+         
+        ans: 3,
+    },
+    {
+        question: " The correct sequence of HTML tags for starting a webpage is",
+        option1: "Head, Title, HTML, body",
+        option2: "HTML, Body, Title, Head",
+        option3: "HTML, Head, Body, Title",
+        option4: "HTML, Head, Title, Body",
+         
+        ans: 4,
+    },
+    {
+        question: " Which of the following tag is used for inserting the largest heading in HTML?",
+        option1: "h4",
+        option2: "h3",
+        option3: "h2",
+        option4: "h1",
+         
+        ans: 4,
+    },
+    {
+        question: " Which of the following element is responsible for making the text bold in HTML?",
+        option1: "pre",
+        option2: "a",
+        option3: "b",
+        option4: "br",
+         
+        ans: 3,
+    },
+    {
+        question: " How to create an unordered list (a list with the list items in bullets) in HTML?",
+        option1: "ul",
+        option2: "ol",
+        option3: "li",
+        option4: "lo",
+         
+        ans: 1,
+    },
+]
